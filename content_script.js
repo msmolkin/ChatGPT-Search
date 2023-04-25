c7cbb4763a77d07653a15668d5373ff7bfb98546
Michael <michael@smolkin.org>
2023-04-25 11:46:54 -0700

    Make save chat code modular, update permissions
    i.e. created a getCurrentChat function that scrapes the chat on the open page
    also changed permissions to allow the extension to access OpenAI.com (otherwise it was useless)

--- content_script.js
+++ content_script.js
@@ -1,25 +1,32 @@
-// Save content from one chat
-// Select all elements with the class names that contain the messages
-const messageElements = document.querySelectorAll(".prose"); // ".border-b, .prose"
-// Later: might need to check whether this only provides assistant responses
+function getCurrentChat() {
+  // Save content from one chat
+  // Select all elements with the class names that contain the messages
+  const messageElements = document.querySelectorAll(".prose"); // ".border-b, .prose"
+  // Later: might need to check whether this only provides assistant responses
 
-// Initialize the chat object
-var current_chat = {};
-current_chat.url = window.location.href;
-current_chat.chat_id = current_chat.url.substring(current_chat.url.indexOf("c/")).substring(2);
-let paid = document.body.innerHTML.match(/Model: (Default|GPT-4|Legacy)( \(?GPT-3.5\)?)?/g);
-current_chat.model = paid ? paid[0] : "Model: Free (Legacy GPT-3.5)";
+  // Initialize the chat object
+  var currentChat = {};
+  currentChat.url = window.location.href;
+  currentChat.chat_id = currentChat.url.substring(currentChat.url.indexOf("c/")).substring(2);
+  let paid = document.body.innerHTML.match(/Model: (Default|GPT-4|Legacy)( \(?GPT-3.5\)?)?/g);
+  currentChat.model = paid ? paid[0] : "Model: Free (Legacy GPT-3.5)";
 
-// Initialize an empty array to store the messages
-current_chat.messages = [];
+  // Initialize an empty array to store the messages
+  currentChat.messages = [];
 
-// Loop through the selected elements and extract their text content
-messageElements.forEach(element => {
-  const message = element.textContent.trim();
-  if (message !== '') {
-    current_chat.messages.push(message);
-  }
-});
+  // Loop through the selected elements and extract their text content
+  messageElements.forEach(element => {
+    const message = element.textContent.trim();
+    if (message !== '') {
+      currentChat.messages.push(message);
+    }
+  });
 
-console.log(current_chat.messages);
-// Expected output: ["User: `test prompt`", "Assistant: `example response`"]
+  // console.log(currentChat.messages);
+  // // Expected output: ["User: `test prompt`", "Assistant: `example response`"]
+
+  return currentChat;
+}
+
+all_chats = []
+all_chats.push(getCurrentChat());
