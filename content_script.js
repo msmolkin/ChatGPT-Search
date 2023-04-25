606a5053ccea87da0839314e050a5d9db2f8a79a
Michael <michael@smolkin.org>
2023-04-25 10:54:26 -0700

    Compile responses from one chat to search through

--- content_script.js
+++ content_script.js
@@ -1,18 +1,25 @@
-// // This code is executed when the add-on is loaded.
-// function onLoad() {
-//   // Get the search input element.
-//   var searchInput = document.getElementById("search-input");
+// Save content from one chat
+// Select all elements with the class names that contain the messages
+const messageElements = document.querySelectorAll(".prose"); // ".border-b, .prose"
+// Later: might need to check whether this only provides assistant responses
 
-//   // Listen for changes to the search input.
-//   searchInput.addEventListener("input", function() {
-//     // Search through the chat for the given text.
-//     var matches = this.value.match(new RegExp(this.value, "i"));
-//     for (var i = 0; i < matches.length; i++) {
-//       var match = matches[i];
-//       var span = document.createElement("span");
-//       span.textContent = match;
-//       span.style.color = "red";
-//       this.parentNode.appendChild(span);
-//     }
-//   });
-// }
+// Initialize the chat object
+var current_chat = {};
+current_chat.url = window.location.href;
+current_chat.chat_id = current_chat.url.substring(current_chat.url.indexOf("c/")).substring(2);
+let paid = document.body.innerHTML.match(/Model: (Default|GPT-4|Legacy)( \(?GPT-3.5\)?)?/g);
+current_chat.model = paid ? paid[0] : "Model: Free (Legacy GPT-3.5)";
+
+// Initialize an empty array to store the messages
+current_chat.messages = [];
+
+// Loop through the selected elements and extract their text content
+messageElements.forEach(element => {
+  const message = element.textContent.trim();
+  if (message !== '') {
+    current_chat.messages.push(message);
+  }
+});
+
+console.log(current_chat.messages);
+// Expected output: ["User: `test prompt`", "Assistant: `example response`"]
