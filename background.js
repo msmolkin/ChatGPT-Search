430db4bb46526f1f53207aaa6596c577d12dbf95
Michael <michael@smolkin.org>
2023-05-08 14:54:46 -0700

    Add checking whether GPT has finished responding

--- background.js
+++ background.js
@@ -1,6 +1,31 @@
-window.onload = function() {
-    console.log("background works, but it loads on its own separate page.");
+window.onload = function () {
+    console.log("background works, but it loads on its own separate page (Inspect add-on to see console.logs).");
     // unnecessary file. can delete this file and remove it from manifest.json
     // once the extension is working. just helpful to see in about:debugging#/runtime/this-firefox:
     // either "Background script: Running" or "Background script: Stopped"
 }
+
+// Listen for messages from content scripts
+
+/**
+ * @param {Object} message
+ * @param {Object} sender
+ * @param {Function} sendResponse
+ * @returns {Promise<void>}
+ * 
+ * Badge the app icon with "typing…" when ChatGPT is generating a response.
+ */
+browser.runtime.onMessage.addListener((message, sender) => {
+    if (message.type === "updateGeneratingState") {
+        console.log("Message received from content script:", message.data);
+
+        if (Boolean(message.data)) {
+            badgeText = "typing…"
+         } else {
+            badgeText = "";
+            console.log("badgeText: ", badgeText)
+         }
+        browser.browserAction.setBadgeText({ text: badgeText });
+        browser.browserAction.setBadgeBackgroundColor({ color: "#00FF00" });
+    }
+});
--- content_script.js
+++ content_script.js
@@ -174,3 +174,56 @@
 
 //   return currentChat;
 // }
+
+/**
+ * Notifies the user whether GPT is still generating a response.
+ */
+var checkWhetherGptTyping = function () {
+
+    const buttons = document.querySelectorAll("button.btn-neutral");
+    let gptGeneratingButton;    // should be const
+    if (buttons && buttons.length > 0) {
+        for (let button of buttons) {
+            if (button.innerText === 'Stop generating' || button.innerText === 'Regenerate response') {
+                gptGeneratingButton = button;
+                break;
+            }
+        }
+    }
+
+    const isGenerating = gptGeneratingButton && gptGeneratingButton.getAttribute('playing') === 'true';
+    let counter = 0; let showCounter = false;    // Counter to count how long it takes to generate a response
+    
+    if (gptGeneratingButton) {
+        if (!isGenerating && gptGeneratingButton.innerText === 'Stop generating') {
+            // This is the case where the user has clicked "Regenerate response" and the response is being generated
+            gptGeneratingButton.setAttribute('playing', 'true');
+            counter++; showCounter = true;
+        } else if (!isGenerating && gptGeneratingButton.innerText === 'Regenerate response') { // TODO: why does this work?
+            // This is the case where the user has clicked "Stop generating" or the response has been generated
+            gptGeneratingButton.setAttribute('playing', 'false');
+
+            // I can inject the time that it took to generate a response into the page here.
+            if (showCounter){
+                console.log(`Response time: ${counter} seconds`);
+                counter = 0;
+                showCounter = false;
+            }
+        }   // Todo: add a case for when the user gave too much text input and we have an error (the page must be refreshed)
+
+
+        // Send a message to the background script
+        browser.runtime.sendMessage({ type: "updateGeneratingState", data: gptGeneratingButton.getAttribute('playing') === 'true' });
+        console.log("Is playing: " + gptGeneratingButton.getAttribute('playing'));
+} else {
+        console.log('Could not find the button element for whether GPT is currently generating content (maybe you\'ve opened a new chat).');
+    }
+
+}
+
+// Possibly todo later:
+// if the user is in another tab, alert the user when GPT has finished typing
+// this will only run if the user checks the box in the options page (requires adding the option to the options page, checked by default)
+
+// Call updateNotification function every 500ms
+setInterval(checkWhetherGptTyping, 1000);
